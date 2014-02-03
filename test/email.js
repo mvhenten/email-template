2d04c202fe30b1f5f3453703148daa55f69a48f7
Matthijs van Henten <matthijs@ischen.nl>
2014-02-03 14:10:45 +0100

    add test for .send

---
+++ test/email.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var Email = require('../lib/email'),
+    diff = require('assert-diff'),
+    assert = require('assert'),
+    Faker = require('Faker');
+
+
+suite(__filename, function() {
+
+    test('Email.send must render a template', function(done) {
+        var file = Date.now().toString(36),
+            from = Faker.Internet.email(),
+            to = Faker.Internet.email(),
+            subject = Faker.Lorem.sentence(),
+            locals = {
+                words: Faker.Lorem.words()
+            };
+
+        var mail = Email.create({
+            template: file + '.html',
+            transport: {
+                send: function(args, done) {
+                    done(null, args);
+                }
+            },
+
+            engine: {
+                render: function(path, locals, done) {
+                    assert.equal(path, file + '.html');
+                    done(null, '<b>' + locals.words.join(', ') + '</b>');
+                }
+            }
+        });
+
+        mail.send(to, from, subject, locals, function(err, data) {
+            var expect = {
+                from: from,
+                to: to,
+                subject: subject,
+                html: '<b>' + locals.words.join(', ') + '</b>',
+                text: locals.words.join(', ')
+            };
+
+
+            diff.deepEqual(data, expect, 'send invoked template rendering and transport.send');
+            done();
+        });
+    });
+});
